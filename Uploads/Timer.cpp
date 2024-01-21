bbcf4f512d1af5719e5ecc1d278b04330c9f59a1
3OmaR_Hacker <112257263+3Mory12@users.noreply.github.com>
2024-01-21 19:35:57 +0200

    Update Timer.cpp

--- Uploads/Timer.cpp
+++ Uploads/Timer.cpp
@@ -7,10 +7,10 @@ int main() {
 
     int start, days = 0, hrs = 0, min = 0, sec = 0;
 
-    cout<< "\t\t\t\t\t ******************* " << endl
+    cout << "\t\t\t\t\t ******************* " << endl
         << "\t\t\t\t\t *      Timer      * " << endl
         << "\t\t\t\t\t ******************* " << endl;
-    
+
     cerr << " If You Want To Stop Timer Press { Ctrl C } " << char(3) << endl;
     cout << " Enter \'1\' To Start The Timer : ";
     cin >> start;
@@ -30,10 +30,12 @@ int main() {
                 days++;
             }
 
-            cout<< "\n\n\n\n\t\t\t\t\t\t" << setw(2) << setfill('0') << days << " : "
-                                          << setw(2) << setfill('0') << hrs << " : "
-                                          << setw(2) << setfill('0') << min << " : "
-                                          << setw(2) << setfill('0') << sec << endl;
+            cout << "\n\n\n\n\n\n     \t\t\t\t\t" << "Days : Hours : Minutes : Seconds " << endl;
+
+            cout << "\n\t\t\t\t\t\t" << setw(2) << setfill('0') << days << " : "
+                << setw(2) << setfill('0') << hrs << " : "
+                << setw(2) << setfill('0') << min << " : "
+                << setw(2) << setfill('0') << sec << endl;
             Sleep(1000);
             sec++;
         }
